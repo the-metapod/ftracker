07685134dc3eac76fd12e9749e59b737c88c9e03
the-metapod <novitzkimain@gmail.com>
2022-06-14 00:05:33 +0200

    Add main class

---
+++ src/main/java/org/metapod/ftracker/FTrackerApplication.java
@@ -0,0 +1,11 @@
+package org.metapod.ftracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FTrackerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FTrackerApplication.class, args);
+	}
+}
